537f4cc1ae06451bbe33316400ebbf2b061c3c4e
Mohamed Moughamir <moughamir@gmail.com>
2016-03-16 20:35:56 +0000

    adding conditions

--- img2vid.sh
+++ img2vid.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+#
 # Create a directory and copy the original images there for manipulation:
+imageRes=640x480
+output="CommitSnap"
+command_exists () {
+    type "$1" &> /dev/null ;
+}
 mkdir temp
 cp *.JPG temp/.
 # Resize the images:
-mogrify -resize 800x600  temp/*.JPG
+if command_exists mogrify; then
+	mogrify -resize $imageRes  temp/*.JPG
+else
+	print "mogrify does not exist\n run sudo apt-get install mogrify"
+fi
+
 # Create the morph images
-convert temp/*.JPG -delay 10 -morph 5 temp/%05d.jpg
+if command_exists convert; then
+	convert temp/*.JPG -delay 10 -morph 5 temp/%05d.jpg
+else
+	print "convert does not exist\n run sudo apt-get install convert"
+fi
+
 # Stitch them together into a video
-ffmpeg -r 50 -qscale 2  -i temp/%05d.jpg output.mp4
+if which ffmpeg >/dev/null; then
+	ffmpeg -r 50 -qscale 2  -i temp/%05d.jpg "$output".mp4
+else
+	print "ffmpeg does not exist\n run sudo apt-get install ffmpeg"
+fi
