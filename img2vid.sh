493583eb7c5fba2a75b879f2e2bfbf5669c2cbc9
Mohamed Moughamir <moughamir@gmail.com>
2016-03-16 20:38:44 +0000

    I don't know

--- img2vid.sh
+++ img2vid.sh
@@ -12,19 +12,19 @@ cp *.JPG temp/.
 if command_exists mogrify; then
 	mogrify -resize $imageRes  temp/*.JPG
 else
-	print "mogrify does not exist\n run sudo apt-get install mogrify"
+	print "mogrify does not exist\n run sudo apt-get install ImageMagick"
 fi
 
 # Create the morph images
 if command_exists convert; then
 	convert temp/*.JPG -delay 10 -morph 5 temp/%05d.jpg
 else
-	print "convert does not exist\n run sudo apt-get install convert"
+	print "convert does not exist\n run sudo apt-get install ImageMagick"
 fi
 
 # Stitch them together into a video
 if which ffmpeg >/dev/null; then
 	ffmpeg -r 50 -qscale 2  -i temp/%05d.jpg "$output".mp4
 else
-	print "ffmpeg does not exist\n run sudo apt-get install ffmpeg"
+	print "ffmpeg does not exist\n run sudo apt-get install ImageMagick"
 fi
