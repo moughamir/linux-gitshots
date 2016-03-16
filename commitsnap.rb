b20d21429102696a99da13e6df1bb30f58c9c20a
Mohamed Moughamir <moughamir@gmail.com>
2016-03-16 20:18:21 +0000

    Few change

--- commitsnap.rb
+++ commitsnap.rb
@@ -6,11 +6,11 @@ require 'fileutils'
 fswebcam_path = `which fswebcam`.strip
 exit 0 if fswebcam_path.empty?
 
-print "Snapshotting your pretty mug..."
-
-
 base_path = File.expand_path("~/.gitshots")
 file = File.join(base_path, "#{Time.now.to_f}.jpg")
+
+print "Snapshotting your pretty mug into #{file}!"
+
 
 FileUtils.mkdir_p(base_path)
 system("#{fswebcam_path} -r 640x480 --jpeg 85 #{file} -q")
